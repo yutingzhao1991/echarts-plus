d28169df9103e77c45e0c23f2e2f0fde0f3f6ebc
Tingzhao Yu <yutingzhao1991@sina.com>
2017-03-02 18:10:33 +0800

    init new version

---
+++ src/transfer/config.js
@@ -0,0 +1,29 @@
+export const coordOption = {
+  rect: {
+  },
+  map: {
+  }
+}
+
+export const seriesType = {
+  rect: 'bar',
+  polar: 'pie',
+  map: 'map'
+}
+
+export const visionsOrder = {
+  x: 0,
+  y: 1,
+  angle: 0,
+  size: 2,
+  color: 3,
+  name: -1
+}
+
+export const seriesOption = {
+  scatter: {
+  },
+  map: {
+    mapType: 'china'
+  }
+}
--- src/transfer/index.js
+++ src/transfer/index.js
@@ -1,47 +1,175 @@
 /**
- * 通用图表，用于描述最常规的可视化图表
+ * EChartsPlus option transfer
+ * 将 EChartsPlus 的配置转化为 ECharts 的配置
  */
-import { merge } from 'lodash'
-import rectCoord from './rect' // 直角坐标系
-import polarCoord from './polar' // 极坐标系
-import mapCoord from './map' // 地理坐标系
+import _ from 'lodash'
+import { coordOption, visionsOrder, seriesOption, seriesType } from './config'
 
-/**
- * build
- * @param {Array} data - 数据，最近48个小时的数据
- * @param {Object} config - 配置
- * @param {Array} config.visions - 视觉配置
- * @param {Object} [extOption] - 自定义的Echart配置
- * @returns {Object} ECharts的配置
- */
-export default function build (data, config, extOption) {
-  // 初始化视觉通道所需要的配置
-  // 参考g2的coord坐标系定义 https://antv.alipay.com/g2/doc/tutorial/start/coord.html
-  var opt
-  extOption = extOption || config.extOption
-  if (config.coord == 'rect') {
-    opt = rectCoord(data, config)
-  } else if (config.coord == 'polar') {
-    opt = polarCoord(data, config)
-  } else if (config.coord == 'map') {
-    opt = mapCoord(data, config)
-  } else {
-    opt = {}
-    console.error('coord: ' + config.coord + ' is illegal')
+export default function build (data, config) {
+  var opt = {}
+  // 初始化类目轴相关配置信息
+  // categoryInfo like this:
+  // {
+  //   option: {
+  //     xAxis: [],
+  //     yAxis: []
+  //   },
+  //   indexMap: {
+  //     xxx_field: {
+  //       xxxx: 1
+  //     }
+  //   }
+  // }
+  var categoryInfo = generateCategoryInfo(data, config)
+  opt = _.merge(opt, categoryInfo.option)
+  var categoryIndexMap = categoryInfo.indexMap || {}
+
+  // 生成series
+  var series = generateSeries(data, config, config.series, categoryIndexMap)
+  opt.series = series
+
+  // 生成legend
+  var legends = []
+  if (config.legendTarget === 'series') {
+    legends = _.chain(series).map('name').compact().uniq().value()
+  } else if (config.legendTarget === 'item') {
+    legends = _.chain(series).map('data').flatten().map('name').compact().uniq().value()
+  }
+  if (legends.length > 0) {
+    opt.legend = {
+      show: true,
+      data: legends
+    }
   }
 
-  opt = merge(opt, {
-    tooltip: {
-      show: true
-    },
-    _height: config.height || 320 // 这个高度不是ehcart中的设置，最后会应用到clib/vues/Echarts的height属性
-  })
-  
-  // merge用户自定义的echarts配置
-  if (extOption) {
-    opt = merge(opt, extOption)
+  // 生成 visualMap
+  // TODO
+  var colorVision = _.chain(config.series).map('visions').flatten().find({ channel: 'color' }).value()
+  if (colorVision) {
+    opt.visualMap = {
+      min: _.minBy(data, colorVision.field)[colorVision.field],
+      max: _.maxBy(data, colorVision.field)[colorVision.field],
+      left: 'left',
+      top: 'bottom',
+      text: ['高','低'],
+      calculable: true
+    }
   }
+
+  // 合并默认配置
+  opt = _.merge(opt, coordOption[config.coord])
+  opt = _.merge(opt, config.option)
   return opt
+}
+
+function generateCategoryInfo (data, config, channel) {
+  if (!channel) {
+    var xInfo = generateCategoryInfo(data, config, 'x')
+    var yInfo = generateCategoryInfo(data, config, 'y')
+    return {
+      option: {
+        xAxis: xInfo.axis,
+        yAxis: yInfo.axis
+      },
+      indexMap: _.assign(xInfo.indexMap, yInfo.indexMap)
+    }
+  }
+
+  var indexMap = {}
+  var valueTranslator = config.valueTranslator || defaultValueTranslator
+
+  var userOption = config.option || {}
+  var axis = config[channel + 'Axis'] || []
+  if (!_.isArray(axis)) {
+    axis = [axis]
+  }
+  var visions = _.chain(config.series).map('visions').flatten().filter({ channel: channel }).value()
+  visions.forEach((v) => {
+    var axisIndex = (v.option && v.option[channel + 'AxisIndex']) || 0
+    axis[axisIndex] = axis[axisIndex] || {
+      type: channel === 'x' ? 'category' : 'value'
+    }
+    if (axis[axisIndex].type === 'category') {
+      var categories = _.chain(data).uniqBy(v.field).sortBy(v.field).value()
+      var categoriesName = []
+      var indexMapItem = {}
+      categories.forEach(function (c, index) {
+        indexMapItem[c[v.field]] = index
+        categoriesName.push(valueTranslator(v.field, c[v.field], c))
+      })
+      axis[axisIndex].data = categoriesName
+      indexMap[v.field] = indexMapItem
+    }
+  })
+  return {
+    axis: visions.length > 0 ? axis : null,
+    indexMap: indexMap
+  }
+}
+
+function generateSeries (data, config, seriesConfig, categoryIndexMap) {
+  var itemTranslator = config.itemTranslator || defaultItemTranslator
+  var valueTranslator = config.valueTranslator || defaultValueTranslator
+  var series = _.chain(seriesConfig).map((sConfig) => {
+    if (sConfig.generator) {
+      // 需要循环生成多个series
+      return _.chain(data).groupBy(sConfig.generator).map((group, key) => {
+        var subConfig = {
+          name: valueTranslator(sConfig.generator, key, group[0])
+        }
+        _.assign(subConfig, sConfig)
+        subConfig.generator = null
+        return generateSeries(group, config, [subConfig], categoryIndexMap)[0]
+      }).value()
+    }
+    var visions = _.chain(sConfig.visions).filter((v) => {
+      return v.channel !== 'name'
+    }).sort((a, b) => {
+      return visionsOrder[a.channel] - visionsOrder[b.channel]
+    }).value()
+    var nameVision = _.find(sConfig.visions, { channel: 'name' })
+    console.log(sConfig.visions, nameVision)
+    var s = {}
+    s.name = sConfig.name || (sConfig.option && sConfig.option.name)
+    s.data = data.map((item) => {
+      var value = visions.map((v) => {
+        if (categoryIndexMap[v.field]) {
+          return categoryIndexMap[v.field][item[v.field]]
+        } else {
+          return item[v.field]
+        }
+      })
+      var name = itemTranslator(item)
+      if (name == null && nameVision) {
+        name = valueTranslator(nameVision.field, item[nameVision.field], item)
+      }
+      return {
+        name: name || s.name,
+        value: value
+      }
+    })
+    s.type = sConfig.type || seriesType[config.coord]
+    s = _.merge(s, sConfig.option)
+    s = _.merge(s, getSeriesDefaultOption(s.type))
+    return s
+  }).value()
+  return _.flatten(series)
+}
+
+function getSeriesDefaultOption (type) {
+  return seriesOption[type]
+}
+
+function defaultItemTranslator (item) {
+  return item.name || null
+}
+
+function defaultValueTranslator (field, value, item) {
+  if (item && item[field + '__name']) {
+    return item[field + '__name']
+  } else {
+    return value
+  }
 }
 
 module.exports = exports['default']
