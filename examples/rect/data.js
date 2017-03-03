f166899544d4921025871f9568600c451783e42f
Tingzhao Yu <yutingzhao1991@sina.com>
2017-03-03 15:20:25 +0800

    add examples

---
+++ examples/rect/data.js
@@ -0,0 +1,21 @@
+var data = [{
+  dt: '2017-01-01',
+  platform_id: '1',
+  platform: 'PC端',
+  revenue: 160
+}, {
+  dt: '2017-01-01',
+  platform_id: '2',
+  platform: '移动端',
+  revenue: 120
+}, {
+  dt: '2017-01-02',
+  platform_id: '1',
+  platform: 'PC端',
+  revenue: 170
+}, {
+  dt: '2017-01-02',
+  platform_id: '2',
+  platform: '移动端',
+  revenue: 96
+}]
